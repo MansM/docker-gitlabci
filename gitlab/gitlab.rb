a217c23921f7081f2179e23369baaf0cb6a52d83
Mans Matulewicz <mans.matulewicz@gmail.com>
2017-07-21 16:12:04 +0200

    initial

---
+++ gitlab/gitlab.rb
@@ -0,0 +1,5 @@
+## Latest options listed at https://gitlab.com/gitlab-org/omnibus-gitlab/blob/master/files/gitlab-config-template/gitlab.rb.template
+
+gitlab_rails['gitlab_shell_ssh_port'] = 10022
+gitlab_rails['initial_root_password'] = "password"
+gitlab_rails['initial_shared_runners_registration_token'] = "rv5s57hsXxPNBXQghPV3"
